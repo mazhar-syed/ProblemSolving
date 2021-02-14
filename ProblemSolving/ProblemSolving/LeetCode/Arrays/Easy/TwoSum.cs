1f43bef2e2491afd13e159d04444576b0eff0fa7
Mazhar <mazhar_syed@outlook.com>
2021-02-15 00:59:25 +0530

    First program on Arrays

---
+++ ProblemSolving/ProblemSolving/LeetCode/Arrays/Easy/TwoSum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//https://leetcode.com/problems/two-sum/
+namespace ProblemSolving.LeetCode.Arrays.Easy
+{
+    public class TwoSum1
+    {
+        public static void Execute()
+        {
+            Console.WriteLine(string.Join(",", TwoSum(new int[] { 2, 7, 11, 15 }, 9))); //0,1
+            Console.WriteLine(string.Join(",", TwoSum(new int[] { 3,2,4 }, 6)));    //1,2
+            Console.WriteLine(string.Join(",", TwoSum(new int[] { 3,3 }, 6)));  //0,1
+
+            Console.WriteLine(string.Join(",", TwoSum(new int[] { -3, 8, -5, 7, 100, 14, 17 }, 9))); //2,5
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var valueIndexMap = new Dictionary<int, List<int>>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (valueIndexMap.ContainsKey(nums[i]))
+                    valueIndexMap[nums[i]].Add(i);
+                else
+                    valueIndexMap.Add(nums[i], new List<int>() { i });
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var diff = target - (nums[i]);
+                if (valueIndexMap.ContainsKey(diff))
+                {
+                    foreach(var index in valueIndexMap[diff])
+                        if(index != i)
+                            return new int[] { i, index };
+                }
+            }
+
+            return new int[] { -1, -1 };
+        }
+    }
+}
--- ProblemSolving/ProblemSolving/LeetCode/LeetCodeMain.cs
+++ ProblemSolving/ProblemSolving/LeetCode/LeetCodeMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProblemSolving.LeetCode.Arrays.Easy;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,7 @@ namespace ProblemSolving.LeetCode
         public static void Run()
         {
             Console.WriteLine("Running LeetCode problems...");
+            TwoSum1.Execute();
         }
     }
 }
