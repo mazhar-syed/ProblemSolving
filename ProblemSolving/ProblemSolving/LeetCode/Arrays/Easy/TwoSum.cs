9707e13bf746985ddb8237a9d4781ebced944a4e
Mazhar <mazhar_syed@outlook.com>
2021-02-15 23:45:30 +0530

    Solved TwoSum problem with a different approach

--- ProblemSolving/ProblemSolving/LeetCode/Arrays/Easy/TwoSum.cs
+++ ProblemSolving/ProblemSolving/LeetCode/Arrays/Easy/TwoSum.cs
@@ -10,13 +10,30 @@ namespace ProblemSolving.LeetCode.Arrays.Easy
         public static void Execute()
         {
             Console.WriteLine(string.Join(",", TwoSum(new int[] { 2, 7, 11, 15 }, 9))); //0,1
-            Console.WriteLine(string.Join(",", TwoSum(new int[] { 3,2,4 }, 6)));    //1,2
-            Console.WriteLine(string.Join(",", TwoSum(new int[] { 3,3 }, 6)));  //0,1
+            Console.WriteLine(string.Join(",", TwoSum(new int[] { 3, 2, 4 }, 6)));    //1,2
+            Console.WriteLine(string.Join(",", TwoSum(new int[] { 3, 3 }, 6)));  //0,1
 
             Console.WriteLine(string.Join(",", TwoSum(new int[] { -3, 8, -5, 7, 100, 14, 17 }, 9))); //2,5
+
+            Console.WriteLine(string.Join(",", TwoSum(new int[] { -3, 8, -5, -20, -20, 7, 100, 14, 17 }, 9))); //2,5
         }
 
         public static int[] TwoSum(int[] nums, int target)
+        {
+            var valueIndexMap = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                var diff = target - nums[i];
+                if (valueIndexMap.ContainsKey(diff))
+                {
+                    return new int[] { i, valueIndexMap[diff] };
+                }
+                if (!valueIndexMap.ContainsKey(nums[i])) valueIndexMap.Add(nums[i], i);
+            }
+            return new int[] { };
+        }
+
+        public static int[] TwoSumAttempt1(int[] nums, int target)
         {
             var valueIndexMap = new Dictionary<int, List<int>>();
             for(int i = 0; i < nums.Length; i++)
