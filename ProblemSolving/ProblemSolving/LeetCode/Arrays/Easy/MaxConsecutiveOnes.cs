bdc88720e0a62aee3f05b81f4a53bda78f144576
Mazhar <mazhar_syed@outlook.com>
2021-02-17 00:24:22 +0530

    Solved a problem - finding max consecutive ones in an array

---
+++ ProblemSolving/ProblemSolving/LeetCode/Arrays/Easy/MaxConsecutiveOnes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//https://leetcode.com/problems/max-consecutive-ones/
+namespace ProblemSolving.LeetCode.Arrays.Easy
+{
+    public class MaxConsecutiveOnes
+    {
+        public static void Execute()
+        {
+            Console.WriteLine(FindMaxConsecutiveOnes(new int[] { 1, 1, 0, 1, 1, 1 })); //3
+        }
+
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int max = 0, currMax = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == 1)
+                {
+                    currMax += 1;
+                }
+                else
+                {
+                    if (currMax > max)
+                        max = currMax;
+                    currMax = 0;
+                }
+            }
+            return currMax > max ? currMax : max;
+        }
+    }
+}
